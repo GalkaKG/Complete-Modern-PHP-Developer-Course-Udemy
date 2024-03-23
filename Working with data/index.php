ae8ddc69384aa04326657273dacec5f74403531e
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-23 09:42:31 +0200

    add directory with first theme

---
+++ Working with data/index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p> Hello World</p>'; ?>
+    </body>
+</html>
+
+<h1>
+<?php 
+
+echo "Hello from PHP.";
+echo "  My name is Galina"
+?>
+</h1>
+
+<?php
+
+$age = 20; 
+
+$age = 30;
+
+echo $age;
